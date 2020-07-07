c218cf81bbbb1cdc52199117a5a5bb4d8ee28af8
Maja Francetic <francetic.maja@gmail.com>
2020-07-07 13:07:46 +0200

    Add an onClickListener to the done Button, that displays the nickname in the TextView, and hides the nickname EditText

--- app/src/main/java/com/mfrancetic/aboutme/MainActivity.kt
+++ app/src/main/java/com/mfrancetic/aboutme/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.mfrancetic.aboutme
 
+import android.content.Context
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editText: EditText;
+    lateinit var nicknameTextView: TextView;
+    lateinit var doneButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.nickname_edit);
+        nicknameTextView = findViewById(R.id.nickname_text);
+        doneButton = findViewById(R.id.done_button)
+
+        // it = doneButton
+        doneButton.setOnClickListener { addNickname(it) }
+    }
+
+    private fun addNickname(view: View) {
+        nicknameTextView.text = editText.text;
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE;
+
+        // Hide the keyboard
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
